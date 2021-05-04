c88fe995ddcc61cb5be7f7db085779d33b0c4b0f
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-04 16:54:46 +0900

    バリデーションの登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ApplicationRecord
 
   with_options presence: true do    
     validates :nickname
-    validates :first_name
-    validates :last_name
-    validates :first_name_kana
-    validates :last_name_kana 
+    validates :first_name,
+      format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+    validates :last_name,
+      format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+    validates :first_name_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/}
+    validates :last_name_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/}
     validates :birthday
   end
 end
