c0f768f9b38d08fb31231d5cd93352cbe482b9d4
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-04 16:00:25 +0900

    保存機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-      has_many: items
+      has_many :items
+
+  with_options presence: true do    
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana 
+    validates :birthday
+  end
 end
--- db/migrate/20210504030246_devise_create_users.rb
+++ db/migrate/20210504030246_devise_create_users.rb
@@ -4,13 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,            null: false
+      t.string :nickname,            null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :last_name,           null: false
-      t.string :first_name,          null: false
-      t.string :last_name_kana,      null: false
-      t.string :first_name_kana,     null: false
+      t.string :last_name,           null: false, default: ""
+      t.string :first_name,          null: false, default: ""
+      t.string :last_name_kana,      null: false, default: ""
+      t.string :first_name_kana,     null: false, default: ""
       t.date :birthday,             null: false
 
       ## Recoverable
