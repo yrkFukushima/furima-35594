10e13ba4964b735950185fc4804371946d2327f6
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-05 15:18:17 +0900

    修正#3

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,14 @@ class User < ApplicationRecord
     validates :nickname
     validates :password,
       format: {with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/}
-    validates :first_name,
-      format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
-    validates :last_name,
-      format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
-    validates :first_name_kana,
-    format: { with: /\A([ァ-ン]|ー)+\z/}
-    validates :last_name_kana,
-    format: { with: /\A([ァ-ン]|ー)+\z/}
+    with_options format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/} do
+      validates :first_name
+      validates :last_name
+    end  
+    with_options format: { with: /\A([ァ-ン]|ー)+\z/} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
     validates :birthday
   end
 end
