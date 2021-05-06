d3741e6dd01013d19a5d7a7ec2f3b2b7c31f7779
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-06 10:56:54 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :delivery_time
+  belongs_to :prefecture
+
+  
+
 end
