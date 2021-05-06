5daed62c438de6ff79705991ccf3198cf6d9463c
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-06 11:04:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,13 @@ class Item < ApplicationRecord
   belongs_to :delivery_time
   belongs_to :prefecture
 
-  
-
+  with_options presence:true do
+    validates :name
+    validates :intoroduction
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_time_id
+    validates :price
 end
