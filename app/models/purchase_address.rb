2be60a5b63dc311347b8946051d613641168f0b8
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-10 15:38:49 +0900

    Formオブジェクトの作成

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,3 +1,18 @@
-class PurchaseAdress
+class PurchaseAddress
+  include ActiveModel::Model
 
+  attr_accessor :postal_code, :prefecture_id, :city, :address_line1, :address_line2, :tel, :purchase_id, :user_id, :item_id 
+  
+  with_options poresence:true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :address_line1
+    validates :tel
+  end
+  
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Adress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address_line1: address_line1, address_line2: address_line2, tel:tel, purchase_id :purchase_id)
+  end  
 end  
