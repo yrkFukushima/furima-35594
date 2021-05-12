db136bbf58fd50e10c8dff035e6d62f63edbff25
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-12 14:48:18 +0900

    修正#1

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :user_set
 
   def index
     @item = Item.find(params[:item_id])
@@ -11,7 +12,6 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
-      
       pay_item 
       @purchase_address.save
       redirect_to root_path
@@ -19,9 +19,11 @@ class PurchasesController < ApplicationController
       render :index
     end 
   end
+
+  
   
   def  purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address_line1, :address_line2, :tel, :item_id).merge(user_id: current_user.id, token: params[:token])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address_line1, :address_line2, :tel).merge(user_id: current_user.id, item_id: @item.id,token: params[:token])
   end
 
 
@@ -30,7 +32,19 @@ class PurchasesController < ApplicationController
     Payjp::Charge.create(
       amount: @item.price,  
       card: purchase_params[:token],   
-      currency: 'jpy'                
-  )
+      currency: 'jpy')
   end
+
+  def user_set
+    if @purchase_address.present?
+      redirect_to root_path
+    end  
+    unless user_signed_in?
+      redirect_to root_path
+    end
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end  
+  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
       has_many :items
-      has_one :purchase
+      has_many :purchases
     
 
   with_options presence: true do    
