6e58063236cd66dbdb777a90cd9f1d51ad25e78c
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-18 15:40:33 +0900

    画像のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,6 +27,7 @@ class Item < ApplicationRecord
     validates :price,
     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
     format: {with: /\A[0-9]+\z/}
-    validates :images
+    validates :images,
+    length: {maximum:4}
   end  
 end
