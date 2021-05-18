219f85e8ca98f849a63256710da65113ed6e08f0
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-05-18 09:45:50 +0900

    レイアウト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def new
+    
   @item = Item.new
   end
 
@@ -43,7 +44,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :introduction, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_time_id, :price,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :introduction, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_time_id, :price, images:[]).merge(user_id: current_user.id)
   end
 
   def item_set
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   has_one :purchase
   has_many :comments
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -27,6 +27,6 @@ class Item < ApplicationRecord
     validates :price,
     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
     format: {with: /\A[0-9]+\z/}
-    validates :image
+    validates :images
   end  
 end
